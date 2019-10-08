322252a290bcd5151d3f6418e3ccd1e959316dcc
Victoria Sayevich <VictoriaSayevich@gmail.com>
2019-10-08 13:46:39 +0200

    feat: complete task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack =[];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        if (bracketsConfig[j][1] == stack[stack.length-1] && bracketsConfig[j][0] == bracketsConfig[j][1]) {
+          stack.pop();
+          continue;
+        }
+        stack.push(bracketsConfig[j][0]);
+        continue;
+      }
+      if (str[i] == bracketsConfig[j][1]) {
+        if (stack.pop() != bracketsConfig[j][0]) return false;
+      }
+    }
+  }
+  return !stack.length;
 }
